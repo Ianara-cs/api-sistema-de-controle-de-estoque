d9cac927dc39cad26d6189a02e34b37467ec85cb
Ianara-cs <ianarasilva333@gmail.com>
2023-03-10 14:14:01 -0300

    tratamento de erros

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express'
+import "express-async-errors"
 import "reflect-metadata"
 import { AppError } from '../../errors/AppErrors'
 import { router } from './routes'
